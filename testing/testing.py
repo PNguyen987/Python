e731012cfdc66ca4e98d874bc0e1aa28f7cf4065
PNguyen987 <pnguyen@nevada.unr.edu>
2022-08-18 15:01:33 -0700

    leftover notes that havn't been pushed

--- testing/testing.py
+++ testing/testing.py
@@ -1,9 +1,51 @@
-from datetime import datetime
-odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31]
+# A Python3 program to find value of the
+# deepest node in a given binary tree
+class new_Node:
+	def __init__(self, key):
+		self.data = key
+		self.left = self.right = None
+		
+# Utility function to find height
+# of a tree, rooted at 'root'.
+def height(root):
+	if(not root):
+		return 0
+	
+	leftHt = height(root.left)
+	rightHt = height(root.right)
+	
+	return max(leftHt, rightHt) + 1
 
-right_this_minute = datetime.today().minute
+# levels : current Level
+# Utility function to print all
+# nodes at a given level.
+def deepestNode(root, levels):
+	if(not root):
+		return
+	
+	if(levels == 1):
+		print(root.data)
+	elif(levels > 1):
+		deepestNode(root.left, levels - 1)
+		deepestNode(root.right, levels - 1)
 
-if right_this_minute in odds: 
-    print("odd")
-else:
-    print("even")
+# Driver Code
+if __name__ == '__main__':
+
+	root = new_Node(1)
+	root.left = new_Node(2)
+	root.right = new_Node(3)
+	# root.left.left = new_Node(4)
+	# root.right.left = new_Node(5)
+	# root.right.right = new_Node(6)
+	# root.right.left.right = new_Node(7)
+	# root.right.right.right = new_Node(8)
+	# root.right.left.right.left = new_Node(9)
+	
+	# Calculating height of tree
+	levels = height(root)
+	
+	# Printing the deepest node
+	deepestNode(root, levels)
+
+# This code is contributed by PranchalK
